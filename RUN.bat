534fc106cf39f08a481892e7424794b5ae0976d6
nicholas12392 <87773847+nicholas12392@users.noreply.github.com>
2024-05-31 18:35:34 +0200

    Update RUN.bat

--- RUN.bat
+++ RUN.bat
@@ -7,6 +7,6 @@ echo Temporarily added ENVIRONMENT VARIABLE: %working_dir%
 
 SET PATH=%PATH%;%working_dir%
 
-C:\Users\nicho\PycharmProjects\Test-Venv\venv\Scripts\python.exe image_processing.py
+python.exe image_processing.py
 
 pause
