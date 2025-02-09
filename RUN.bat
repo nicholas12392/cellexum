7d67d8b007a731399aee8166b70121ff86ee5f7c
nicholas12392 <87773847+nicholas12392@users.noreply.github.com>
2025-02-09 19:02:55 +0100

    GUI update

--- RUN.bat
+++ RUN.bat
@@ -1,12 +1,4 @@
 @ECHO OFF
 
-CD > tmpFile
-SET /p working_dir= < tmpFile
-DEL tmpFile
-echo Temporarily added ENVIRONMENT VARIABLE: %working_dir%
-
-SET PATH=%PATH%;%working_dir%
-
-python.exe image_processing.py
-
-pause
+cd src
+python cellexum.py
