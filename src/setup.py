1bf086ba9b07b0710475285b65a951107cd50a95
nicholas12392 <87773847+nicholas12392@users.noreply.github.com>
2025-02-09 19:09:57 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    name='NTSA Img Processing',
-    version='2',
+    name='Cellexum',
+    version='2.0.0',
     author='Nicholas Hansen',
     author_email='nicholas.2000@live.dk',
-    description='A specified pack of software designed to analyze NTSA surfaces.',
+    description='A program designed for analysis of cells on 2D surfaces.',
     license='MIT',
     install_requires=['matplotlib', 'pandas', 'tqdm', 'opencv-python, python-javabridge', 'numpy', 'python-bioformats'],
 )
